9251faf701a7c1f27c29e2254935422ae58913cc
zlobin_ilya <qwertin1998@gmail.com>
2024-11-11 18:42:15 +0300

    Add: redis cache for all products

--- backend/src/services/CacheService.ts
+++ backend/src/services/CacheService.ts
@@ -1,19 +1,29 @@
 import Redis from '@/config/redis';
 
+export enum CacheKey {
+    products = 'allProducts'
+}
 
 export interface ICacheService {
-    get<T>(key: string): Promise<T | null>,
-    set(key: string, value: string | number): Promise<void>,
+    get<T>(key: CacheKey): Promise<T | null>,
+    set(key: CacheKey, value: unknown): Promise<void>,
 }
 
-export class CacheService implements ICacheService {
-    async get<T>(key: string) {
+class RedisCacheService implements ICacheService {
+    async get<T>(key: CacheKey) {
         const data = await Redis.get(key);
-        if (!data) return null;
-        return JSON.parse(data) as T;
+        if (!data) {
+            console.log(`Cache for ${key} not found`);
+            return null
+        };
+        const jsonData = JSON.parse(data) as T
+        console.log(`Get cache for ${key}`, jsonData);
+        return jsonData;
     }
-    async set<T>(key: string, data: string | number) {
-        data = JSON.stringify(data)
-        await Redis.set(key, data)
+    async set<T>(key: CacheKey, data: unknown) {
+        const jsonString = JSON.stringify(data);
+        await Redis.set(key, jsonString, 'EX', 60);
     };
 }
+
+export const CacheService = new RedisCacheService();
