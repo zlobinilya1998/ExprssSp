7c4e03baac0309391c7e00844f718fb022cf7eca
zlobin_ilya <qwertin1998@gmail.com>
2024-11-12 22:50:38 +0300

    Add: dev compose

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -7,8 +7,10 @@ const db: string = env.DB_DATABASE || 'db'
 const user: string = env.DB_USER || 'postgres'
 const pass: string = env.DB_PASS || 'postgres'
 const host: string = env.DB_HOST || 'localhost'
+const port: any = env.DB_PORT;
 export const sequelize = new Sequelize(db, user, pass, {
     host,
+    port: port || 5432,
     dialect: 'postgres',
   });
 
